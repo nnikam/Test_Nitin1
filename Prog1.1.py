89d2703f1c6579c9e8f1224dad393024eeab4c1a
Nitin Nikam <nitin.nikam@CALSOFTHQ.COM>
2016-04-14 10:46:19 +0530

    This is test commit again for prog1.1

---
+++ Prog1.1.py
@@ -0,0 +1,8 @@
+print "Hello Welcome to Python"
+a=10
+b=20
+print "The Addition of 2 numbers is",a+b
+print "The Subtraction of 2 numbers is",a-b
+print "The Multiplication of 2 numbers is",a*b
+print "The Division of 2 numbers is",a/b
+
